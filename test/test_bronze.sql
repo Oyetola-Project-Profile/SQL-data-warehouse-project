5d595b4bc41db291ff47b7b1e68f0e88e7efaee3
Oyetola Ogungbile <oyetolaogungbile@gmail.com>
2026-08-26 18:30:09 -0400

    Add SQL for cleaning and inserting customer info
    Added SQL queries for cleaning and inserting customer data into silver layer.

--- test/test_bronze.sql
+++ test/test_bronze.sql
@@ -1,4 +1,3 @@
-
 --=======================================
 --CHECKING FOR DUPLICATES AND NULLS 
 --In table bronze.crm_cust_info
@@ -74,6 +73,87 @@ SELECT * FROM
 	WHERE cst_id IS NOT NULL	
 
 ) t WHERE flag_last = 1	
+
+
+
+
+
+--=======================================================
+--CLEANING THE TABLE bronze.crm_cust_info
+--ALSO CHECK THE DATE COLUMN TO MAKE SURE IT IS DATE TYPE
+--=======================================================
+
+SELECT [cst_id]
+      ,[cst_key]
+      ,TRIM([cst_firstname])
+      ,TRIM([cst_lastname])
+      ,CASE WHEN cst_marital_status = 'M' THEN 'Married'
+			WHEN cst_marital_status = 'S' THEN 'Single'
+			ELSE 'n/a'
+		END	[cst_marital_status]
+      ,CASE WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
+			WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
+			ELSE 'n/a'
+		END [cst_gndr]
+      ,[cst_create_date]
+  FROM 
+ (
+	SELECT 
+	* ,
+	ROW_NUMBER() OVER(PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag_last
+	FROM bronze.crm_cust_info
+	WHERE cst_id IS NOT NULL	
+
+) t WHERE flag_last = 1	
+
+
+
+
+
+--==================================================
+--INSERTING INTO SILVER LAYER TABLE:
+--silver.crm_cust_info
+--==================================================
+
+
+INSERT INTO silver.crm_cust_info
+(
+	   [cst_id]
+      ,[cst_key]
+      ,[cst_firstname]
+      ,[cst_lastname]
+      ,[cst_marital_status]
+      ,[cst_gndr]
+      ,[cst_create_date]
+)
+
+SELECT [cst_id]
+      ,[cst_key]
+      ,TRIM([cst_firstname])
+      ,TRIM([cst_lastname])
+      ,CASE WHEN cst_marital_status = 'M' THEN 'Married'
+			WHEN cst_marital_status = 'S' THEN 'Single'
+			ELSE 'n/a'
+		END	[cst_marital_status]
+      ,CASE WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
+			WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
+			ELSE 'n/a'
+		END [cst_gndr]
+      ,[cst_create_date]
+  FROM 
+ (
+	SELECT 
+	* ,
+	ROW_NUMBER() OVER(PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag_last
+	FROM bronze.crm_cust_info
+	WHERE cst_id IS NOT NULL	
+
+) t WHERE flag_last = 1	
+
+
+
+SELECT * FROM silver.crm_cust_info
+	
 
 
 --===================================================
